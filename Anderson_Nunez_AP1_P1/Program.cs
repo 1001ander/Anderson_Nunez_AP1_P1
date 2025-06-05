ee8175ae5afe00c57cf4a1033557ef5a7c527f50
Anderson <@1001ander>
2025-06-04 23:20:19 -0400

    Inyectar el contexto en el program.

--- Anderson_Nunez_AP1_P1/Program.cs
+++ Anderson_Nunez_AP1_P1/Program.cs
@@ -1,10 +1,22 @@
 using Anderson_Nunez_AP1_P1.Components;
+using Anderson_Nunez_AP1_P1.DAL;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+//Contexto
+var ConString = builder.Configuration.GetConnectionString("SqlConSrt");
+builder.Services.AddDbContextFactory<Contexto>(o => o.UseSqlServer(ConStr));
+
+void ConStr(SqlServerDbContextOptionsBuilder builder)
+{
+    throw new NotImplementedException();
+}
 
 var app = builder.Build();
 
