a81652d64ffc4b812c63942bd774c6d58aa51805
Anderson <@1001ander>
2025-06-10 00:08:24 -0400

    Crear el contexto.

--- Anderson_Nunez_AP1_P1/DAL/Contexto.cs
+++ Anderson_Nunez_AP1_P1/DAL/Contexto.cs
@@ -1,9 +1,13 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Anderson_Nunez_AP1_P1.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Internal;
 
 namespace Anderson_Nunez_AP1_P1.DAL;
 
 public class Contexto : DbContext 
 {
     public Contexto(DbContextOptions<Contexto> options) : base(options) { }
+
+    public DbSet<Aportes> Aportes { get; set; }
 
 }
