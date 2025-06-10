d2fba75ed5e427693b763db3bf719bade599df2c
Anderson <@1001ander>
2025-06-10 00:07:36 -0400

    Crear el Modelo de aportes.

---
+++ Anderson_Nunez_AP1_P1/Models/Aportes.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace Anderson_Nunez_AP1_P1.Models;
+
+public class Aportes
+{
+    [Key]
+    public int AporteId { get; set; }
+
+    [Required(ErrorMessage = "Campo Fecha obligatorio")]
+    public DateTime Fecha { get; set; } = DateTime.Now;
+
+    [Required(ErrorMessage = "Campo Nombre obligatorio")]
+    [StringLength(100, ErrorMessage = "Máximo 100 caracteres")]
+    public string? Nombre { get; set; }
+
+    [Required(ErrorMessage = "Campo Monto obligatorio")]
+    [Range(1, double.MaxValue, ErrorMessage = "El monto debe ser mayor que 0")]
+    public decimal Monto { get; set; }
+
+}
--- Anderson_Nunez_AP1_P1/Models/Registro.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Anderson_Nunez_AP1_P1.Models;
-
-public class Registro
-{
-    [Key]
-    public int Id { get; set; }
-}
